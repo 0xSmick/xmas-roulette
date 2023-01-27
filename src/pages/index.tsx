f70e3000eb924d2ebb1f74f74cc8454364f0c18c
Sheldon Smickley <sheldon@Sheldons-MacBook-Pro.local>
2023-01-27 18:33:33 -0500

    first commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,91 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '@/styles/Home.module.css'
+import Head from "next/head";
+import dynamic from "next/dynamic";
+import React, { useState } from "react";
+import { Box, VStack } from "@chakra-ui/layout";
+import { Button } from "@chakra-ui/button";
 
-const inter = Inter({ subsets: ['latin'] })
+const colors = [
+  "#000080", // dark blue
+  "#0000b3",
+  "#0000cc",
+  "#0000e6",
+  "#0000ff",
+  "#4d4dff",
+  "#9999ff",
+  "#b3b3ff",
+  "#ccccff",
+  "#e6e6ff",
+  "#ffffff",
+  "#e6e6ff",
+  "#ccccff",
+];
+const data = [
+  {
+    option: "Fancy Olive Oil",
+    style: { textColor: "white" },
+  },
+  {
+    option: "$100 Gift Card",
+    style: { textColor: "white" },
+  },
+  {
+    option: "2 movie dates",
+    style: { textColor: "white" },
+  },
+  {
+    option: "Wine or Beer Subscription",
+    style: { textColor: "white" },
+  },
+  {
+    option: "1 home cooked meal",
+    style: { textColor: "white" },
+  },
+  {
+    option: "$200 Airbnb Gift Card",
+  },
+  {
+    option: "Fancy Olive Oil",
+  },
+  {
+    option: "1 home cooked meal",
+  },
+  {
+    option: "Train ticket & broadway",
+  },
+  {
+    option: "1 home cooked meal",
+  },
+  {
+    option: "Car oil change",
+  },
+  {
+    option: "$200 gift card",
+  },
+  {
+    option: "Coffee subscription",
+  },
+];
+
+const Wheel = dynamic(
+  () => import("react-custom-roulette").then((mod) => mod.Wheel),
+  { ssr: false }
+);
 
 export default function Home() {
+  const [mustSpin, setMustSpin] = useState(false);
+  const [prizeNumber, setPrizeNumber] = useState(0);
+  const [spinsRemaining, setSpinsRemaining] = useState(3);
+
+  const handleSpinClick = () => {
+    if (spinsRemaining === 0) {
+      alert("No more spins!");
+      return;
+    }
+    const newPrizeNumber = Math.floor(Math.random() * data.length);
+    setPrizeNumber(newPrizeNumber);
+    setMustSpin(true);
+  };
+
   return (
     <>
       <Head>
@@ -14,110 +94,31 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>pages/index.tsx</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
-          </div>
-        </div>
-
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
+      <Box m={"8px"} paddingTop={"8px"}>
+        <VStack spacing={4}>
+          <p style={{ fontSize: "32px" }}>Christmas Gift Roulette</p>
+          <Wheel
+            backgroundColors={colors}
+            mustStartSpinning={mustSpin}
+            prizeNumber={prizeNumber}
+            data={data}
+            onStopSpinning={() => {
+              setMustSpin(false);
+              setSpinsRemaining(spinsRemaining - 1);
+            }}
+            fontSize={12}
           />
-          <div className={styles.thirteen}>
-            <Image
-              src="/thirteen.svg"
-              alt="13"
-              width={40}
-              height={31}
-              priority
-            />
-          </div>
-        </div>
-
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Find in-depth information about Next.js features and&nbsp;API.
+          <Button colorScheme="blue" size="lg" onClick={handleSpinClick}>
+            SPIN
+          </Button>
+          <p style={{ fontSize: "22px" }}>Spins Remaining: {spinsRemaining}</p>
+          {prizeNumber && !mustSpin && (
+            <p style={{ fontSize: "22px" }}>
+              Current Prize: {data[prizeNumber].option}
             </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
+          )}
+        </VStack>
+      </Box>
     </>
-  )
+  );
 }
